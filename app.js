601f3231cc65f1f986b9e5585d72a9c87582951d
g g <gabedotgutierrez@gmail.com>
2024-09-10 17:19:25 -0400

    finished level ups

--- app.js
+++ app.js
@@ -217,50 +217,220 @@ firstLast = people.map((person) => {
     return firstLast = lastFirst[1] + " " + lastFirst[0]
 })
 
+// // Check your work:
+// console.log('Exercise 5 my result: ', firstLast)
+// console.log(
+//   'Exercise 5 correct result: ',
+//   [
+//     'Carl Becker',
+//     'Samuel Beckett',
+//     'Mick Beddoes',
+//     'Henry Beecher',
+//     'Ludwig Beethoven',
+//     'Menachem Begin',
+//     'Hilaire Belloc',
+//     'Saul Bellow',
+//     'Robert Benchley',
+//     'Peter Benenson',
+//     'David Ben-Gurion',
+//     'Walter Benjamin',
+//     'Tony Benn',
+//     'Chester Bennington',
+//     'Leana Benson',
+//     'Silas Bent',
+//     'Lloyd Bentsen',
+//     'Ric Berger',
+//     'Ingmar Bergman',
+//     'Luciano Berio',
+//     'Milton Berle',
+//     'Irving Berlin',
+//     'Eric Berne',
+//     'Sandra Bernhard',
+//     'Yogi Berra',
+//     'Halle Berry',
+//     'Wendell Berry',
+//     'Erin Bethea',
+//     'Aneurin Bevan',
+//     'Ken Bevel',
+//     'Joseph Biden',
+//     'Ambrose Bierce',
+//     'Steve Biko',
+//     'Josh Billings',
+//     'Frank Biondo',
+//     'Augustine Birrell',
+//     'Elk Black',
+//     'Robert Blair',
+//     'Tony Blair',
+//     'William Blake'
+//   ]
+// )
+
+/*
+Exercise 6: Array.prototype.some()
+
+Determine if there is at least one person in the devs array who is 18 years 
+old or older.
+
+- You have an array of people with their respective ages.
+- Use the Array.prototype.some() method to check if any person in the array is 
+  18 years old or older.
+- Store the result (true or false) in the variable 'isAdultPresent'. 
+*/
+
+let isAdultPresent = null
+
+// Complete the exercise in the space below:
+
+isAdultPresent = devs.some((dev)=>{
+  return   2024 - dev.year >= 18
+})
+
+
+// // Check your work:
+// console.log('Exercise 6 my result: ', isAdultPresent)
+// console.log('Exercise 6 correct result: ', true)
+
+/*
+Exercise 7: Array.prototype.every()
+
+Use Array.prototype.every() to determine if every person in the devs array is 
+19 years old or older.
+
+- You have an array of individuals, each with their year of birth represented
+  by the 'year' property.
+- Use the Array.prototype.every() method to verify if every individual in the
+  array is at least 19 years old.
+- Store the result (true or false) in the variable 'isEveryone19OrOlder'.
+*/
+
+let isEveryone19OrOlder = null
+
+// Complete the exercise in the space below:
+
+isEveryone19OrOlder = devs.every((dev) => {
+  return 2024 - dev.year >= 19
+})
+
+
+// // Check your work:
+// console.log('Exercise 7 my result: ', isEveryone19OrOlder)
+// console.log('Exercise 7 correct result: ', false)
+
+/*
+Exercise 8: Array.prototype.find()
+
+Use Array.prototype.find() to identify and retrieve the comment object with 
+a specific ID 823423 from an array of comment objects.
+
+- Assign the found comment object to the variable 'commentById'.
+*/
+
+let commentById = {}
+
+// Complete the exercise in the space below:
+
+commentById = comments.find((comment)=>{
+  return comment.id === 823423 
+})
+
+// // Check your work:
+// console.log('Exercise 8 my result: ', commentById)
+// console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 })
+
+/*
+Exercise 9: Array.prototype.findIndex()
+
+Determine the index of the comment that has a specific ID 123523 in an array 
+of comment objects.
+
+- Store the index in the variable 'idx'.
+*/
+
+let idx = null
+
+// Complete the exercise in the space below:
+
+idx = comments.findIndex((c)=>{
+  return c.id === 123523
+})
+
+// // Check your work:
+// console.log('Exercise 9 my result: ', idx)
+// console.log('Exercise 9 correct result: ', 3)
+
+/*
+Level Up exercise 1: Array.prototype.reduce()
+
+Calculate the combined lifespan of all the inventors using 
+Array.prototype.reduce()
+
+- Each object in the array includes these properties: 
+  'first', 'last', 'year' (birth year), and 'passed' (year of death).
+- Use the Array.prototype.reduce() method to calculate the sum of the total 
+  years lived by all the inventors.
+- Store the total sum in the variable 'totalYearsLived'.
+
+Hints:
+
+- Inside the reduce callback function, calculate the lifespan of each inventor 
+  (passed - year).
+- Accumulate this lifespan in the 'totalYearsLived' variable.
+- Remember, reduce takes a callback function and an initial value for the 
+  accumulator.
+*/
+
+let totalYearsLived = 0
+
+// Complete the exercise in the space below:
+
+totalYearsLived = inventors.reduce((accumulator, currentValue) => {
+  // return (currentValue)
+  return accumulator + (currentValue.passed - currentValue.year)
+  // return ((accumulator.passed - accumulator.year) + currentValue, 0)
+  // why doesn't accumulator.year work?
+},0)
+
+// // Check your work:
+// console.log('Level Up 1 my result: ', totalYearsLived)
+// console.log('Level Up 1 correct result: ', 861)
+
+/*
+Level Up exercise 2: Array.prototype.reduce()
+
+Tallying travel methods using Array.prototype.reduce(). 
+
+Count the number of times each travel method appears in the 'travelMethods'
+array.
+
+- The resulting object should have keys as the travel methods 
+  ('car', 'truck', 'bike', etc.) and values as their respective counts.
+- Store this object in the variable 'travelMethodCounts'.
+
+Hints:
+- Inside the reduce function, check if the travel method already exists as a key
+  in your accumulator object. If it does, increment its count. If not, add it 
+  to the object and give it a value of 1.
+- Since you want to return an object, be sure to pass an empty {} for the 
+  initial value of the "accumulator".
+*/
+
+let travelMethodCounts = {}
+
+// Complete the exercise in the space below:
+travelMethodCounts = travelMethods.reduce(function(acc, method){
+  if(acc[method]){
+    acc[method] = acc[method] + 1;
+  } else {
+    acc[method] = 1;
+  }
+  return acc;
+}, {});
+
+
 // Check your work:
-console.log('Exercise 5 my result: ', firstLast)
+console.log('Level Up 2 my result: ', travelMethodCounts)
 console.log(
-  'Exercise 5 correct result: ',
-  [
-    'Carl Becker',
-    'Samuel Beckett',
-    'Mick Beddoes',
-    'Henry Beecher',
-    'Ludwig Beethoven',
-    'Menachem Begin',
-    'Hilaire Belloc',
-    'Saul Bellow',
-    'Robert Benchley',
-    'Peter Benenson',
-    'David Ben-Gurion',
-    'Walter Benjamin',
-    'Tony Benn',
-    'Chester Bennington',
-    'Leana Benson',
-    'Silas Bent',
-    'Lloyd Bentsen',
-    'Ric Berger',
-    'Ingmar Bergman',
-    'Luciano Berio',
-    'Milton Berle',
-    'Irving Berlin',
-    'Eric Berne',
-    'Sandra Bernhard',
-    'Yogi Berra',
-    'Halle Berry',
-    'Wendell Berry',
-    'Erin Bethea',
-    'Aneurin Bevan',
-    'Ken Bevel',
-    'Joseph Biden',
-    'Ambrose Bierce',
-    'Steve Biko',
-    'Josh Billings',
-    'Frank Biondo',
-    'Augustine Birrell',
-    'Elk Black',
-    'Robert Blair',
-    'Tony Blair',
-    'William Blake'
-  ]
+  'Level Up 2 correct result: ', 
+  { car: 5, truck: 3, bike: 2, walk: 2, van: 2 }
 )
+
